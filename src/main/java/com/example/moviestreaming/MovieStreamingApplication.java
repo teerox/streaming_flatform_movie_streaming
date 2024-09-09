c89c5216d1ab8a43395c85f2148cf659eced64e7
teerox44 <anthony@publiclyme.com>
2024-09-09 18:44:08 -0300

    setup stream service

---
+++ src/main/java/com/example/moviestreaming/MovieStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviestreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieStreamingApplication.class, args);
+	}
+
+}
